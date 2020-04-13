6a1321ccd0181f8a741fec4cb5d07c1df13f400f
hagooog <mohamed.higgy31@gmail.com>
2020-04-13 06:02:32 +0200

    add all pages of tender

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,32 @@
+$(document).ready(function () {
+
+
+  // filter in tenders page
+  $('.nav-filter .list-style .list button').click(function () {
+
+    $(this).addClass('active_btn_filter').siblings().removeClass('active_btn_filter');
+    var CSSFilter = $(this).attr('id');
+    if (CSSFilter === 'all') {
+      $('.Tenders-content > div ').fadeIn();
+    }
+    else {
+      $('.Tenders-content > div ').fadeOut();
+      $('.Tenders-content').contents().filter('.' + CSSFilter).fadeIn();
+    }
+  });
+
+
+  // add another tags
+
+
+  var counter = 0;
+  $('.form-group #add-tag').on('click', function () {
+    counter++;
+    $('#tagsAdded').append('<div class="form-group"><input type="text" class="form-control input-focus" placeholder=" tag ' + counter + ' "></div>');
+  });
+})
+
+
 var emailInput;
 
 function validateEmail(email) {
@@ -17,7 +46,20 @@ $("#email-input").on("change", function () {
   }
 });
 
-/*________________multi step form */
+
+/*hide a navbar when the user starts to scroll the page
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-131px";
+  }
+  prevScrollpos = currentScrollPos;
+} */
+
+/*________________multi step form________________*/
 //jQuery time
 var current_fs, next_fs, previous_fs; //fieldsets
 var left, opacity, scale; //fieldset properties which we will animate
@@ -94,4 +136,90 @@ $(".previous").click(function () {
     //this comes from the custom easing plugin
     easing: 'easeInOutBack'
   });
+});
+
+/*________________slider background________________*/
+
+const track = document.querySelector(".carousel-track");
+const slides = Array.from(track.children);
+
+const nextBtn = document.querySelector(".carousel-btn-right");
+const prevBtn = document.querySelector(".carousel-btn-left");
+
+const dotsNav = document.querySelector(".carousel-nav");
+const dots = Array.from(dotsNav.children);
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+//arrange the slides next to one another
+const setSlidePosition = (slide, index) => {
+  slide.style.left = slideWidth * index + 'px'
+}
+slides.forEach(setSlidePosition);
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+  track.style.transform = "translateX(-" + targetSlide.style.left + ")";
+  currentSlide.classList.remove("current-img-slide");
+  targetSlide.classList.add("current-img-slide")
+}
+
+const updateDots = (currentDot, targetDot) => {
+  currentDot.classList.remove("current-slide")
+  targetDot.classList.add("current-slide")
+}
+
+const hideShowArrows = (slides, prevBtn, nextBtn, targetIndex) => {
+
+  if (targetIndex === 0) {
+    prevBtn.classList.add("is-hidden");
+    nextBtn.classList.remove("is-hidden");
+  } else if (targetIndex === slides.length - 1) {
+    prevBtn.classList.remove("is-hidden");
+    nextBtn.classList.add("is-hidden");
+  } else {
+    prevBtn.classList.remove("is-hidden");
+    nextBtn.classList.remove("is-hidden")
+  }
+}
+
+//click on next and prev btn
+nextBtn.addEventListener("click", e => {
+  const currentSlide = track.querySelector(".current-img-slide");
+  const nextSlide = currentSlide.nextElementSibling;
+  const currentDot = dotsNav.querySelector(".current-slide");
+  const nextDot = currentDot.nextElementSibling;
+  const nextIndex = slides.findIndex(slide => slide === nextSlide);
+  moveToSlide(track, currentSlide, nextSlide)
+  updateDots(currentDot, nextDot)
+
+  hideShowArrows(slides, prevBtn, nextBtn, nextIndex)
+})
+
+prevBtn.addEventListener("click", e => {
+  const currentSlide = track.querySelector(".current-img-slide");
+  const prevSlide = currentSlide.previousElementSibling;
+
+  const currentDot = dotsNav.querySelector(".current-slide");
+  const prevDot = currentDot.previousElementSibling;
+
+  const prevIndex = slides.findIndex(slide => slide === prevSlide);
+
+  moveToSlide(track, currentSlide, prevSlide)
+  updateDots(currentDot, prevDot)
+  hideShowArrows(slides, prevBtn, nextBtn, prevIndex)
+})
+
+//detect the indecators
+dotsNav.addEventListener("click", e => {
+  const targetDot = e.target.closest("button");
+  if (!targetDot) return;
+  const currentSlide = track.querySelector(".current-img-slide");
+  const currentDot = dotsNav.querySelector(".current-slide");
+
+  const targetIndex = dots.findIndex(dot => dot === targetDot);
+  const targetSlide = slides[targetIndex];
+
+  moveToSlide(track, currentSlide, targetSlide);
+  updateDots(currentDot, targetDot);
+  hideShowArrows(slides, prevBtn, nextBtn, targetIndex)
 });
